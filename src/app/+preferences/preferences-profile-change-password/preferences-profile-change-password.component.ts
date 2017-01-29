b490b49e5ca17d47545b129daa8e1dafccdcd990
Long Hồ <Long H>
2017-01-29 17:21:21 +0700

    adding change password form

--- src/app/+preferences/preferences-profile-change-password/preferences-profile-change-password.component.ts
+++ src/app/+preferences/preferences-profile-change-password/preferences-profile-change-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { CustomValidators } from 'ng2-validation';
 
 @Component({
   selector: 'sk-preferences-profile-change-password',
@@ -7,7 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreferencesProfileChangePasswordComponent implements OnInit {
 
-  constructor() { }
+  userChangePasswordForm: FormGroup;
+  oldPassword: FormControl;
+  newPassword: FormControl;
+  repeatNewPassword: FormControl;
+  
+  submitting: boolean = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.oldPassword = this.formBuilder.control('', [Validators.required]);
+    this.newPassword = this.formBuilder.control('', [Validators.required, CustomValidators.rangeLength([6, 15])]);
+    this.repeatNewPassword = this.formBuilder.control('', [Validators.required, CustomValidators.equalTo(this.newPassword)]);
+    this.userChangePasswordForm = this.formBuilder.group({ 
+      oldPassword: this.oldPassword, 
+      newPassword: this.newPassword, 
+      repeatNewPassword: this.repeatNewPassword });
+  }
+
+  onSubmit() {
+    this.submitting = true;
+    this.submitting = false;
+  }
 
   ngOnInit() {
   }
