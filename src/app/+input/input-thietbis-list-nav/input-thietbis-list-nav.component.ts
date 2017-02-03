a3ee51d7968e190b9a6bdb510eb8b6a4abc868c4
Long Hồ <Long H>
2017-02-03 15:00:19 +0700

    prepare css

--- src/app/+input/input-thietbis-list-nav/input-thietbis-list-nav.component.ts
+++ src/app/+input/input-thietbis-list-nav/input-thietbis-list-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'sk-input-thietbis-list-nav',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputThietbisListNavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  getPage(page: number) {
+    this.router.navigate(['/nhap-lieu/thiet-bi'], { queryParams: { page } });
+  }
 
   ngOnInit() {
   }
--- src/app/+input/input.module.ts
+++ src/app/+input/input.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { Ng2PaginationModule } from 'ng2-pagination';
 import { InputRoutingModule, routedComponents } from './input-routing.module';
 import { ThietbisHelpersService } from './shared/thietbis-helpers.service';
 import { ThietbisService } from './shared/thietbis.service';
@@ -8,6 +9,7 @@ import { ThietbisImportService } from './shared/thietbis-import.service';
 @NgModule({
   imports: [
     SharedModule,
+    Ng2PaginationModule,
     InputRoutingModule
   ],
   declarations: [
